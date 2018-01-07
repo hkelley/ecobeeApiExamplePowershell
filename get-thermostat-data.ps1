6c49547ad6d79594976e3fb34f877cf3317820e6
Kelley <hkelley@sompo-intl.com>
2018-01-07 12:54:41 -0500

    added token refresh

--- get-thermostat-data.ps1
+++ get-thermostat-data.ps1
@@ -2,16 +2,29 @@
 
 param
 (
-	    [Parameter(Mandatory = $true)] [string] $TokenFile
-	  , [Parameter(Mandatory = $false)] [string] $BaseUri = "https://api.ecobee.com/"
+	  [Parameter(Mandatory = $true)] [string] $ApiKey
+    , [Parameter(Mandatory = $true)] [string] $TokenFile
+	, [Parameter(Mandatory = $false)] [string] $BaseUri = "https://api.ecobee.com/"
 )
 
 $tokenData = Get-Content -Path $TokenFile | ConvertFrom-Json
-$accessToken= $tokenData.access_token
 
-# TODO  refresh the token
 
-#"grant_type=refresh_token&code=".concat(refreshToken).concat("&client_id=").concat(apiKey);
+# refresh the token
+$uri = ("{0}token?grant_type=refresh_token&refresh_token={1}&client_id={2}" -f $BaseUri,$tokenData.refresh_token,$ApiKey) 
+$uri
+try{
+    $response = Invoke-RestMethod -Uri $uri -Method Post
+} catch {
+    $Error[0]
+    exit
+}
+
+$accessToken = $response.access_token 
+
+# Save the tokens
+$response | ConvertTo-Json | Out-File -FilePath ecobeeToken.json
+
 
 # get the thermostat data
 #`{'selection':`{'selectionType':'registered','selectionMatch':'','includeRuntime':true`}`}
@@ -35,7 +48,7 @@ foreach($t in $jsonResponse.thermostatList)
 
     "runtime"
     $t.runtime | fl *
-
+     
     "events"
     $t.events.count
     $t.events
