c070a12066a40a92c4a2b25bd6f7baa9bc263f89
Kelley <hkelley@sompo-intl.com>
2018-01-07 11:08:45 -0500

    First commit

---
+++ get-thermostat-data.ps1
@@ -0,0 +1,35 @@
+﻿
+
+param
+(
+	    [Parameter(Mandatory = $true)] [string] $TokenFile
+	  , [Parameter(Mandatory = $false)] [string] $BaseUri = "https://api.ecobee.com/"
+)
+
+$tokenData = Get-Content -Path $TokenFile | ConvertFrom-Json
+$accessToken= $tokenData.access_token
+
+# refresh the token
+
+#"grant_type=refresh_token&code=".concat(refreshToken).concat("&client_id=").concat(apiKey);
+
+# get the thermostat data
+#`{'selection':`{'selectionType':'registered','selectionMatch':'','includeRuntime':true`}`}
+
+$uri = "$baseuri/1/thermostat?format=json&body=`{'selection':`{'selectionType':'registered','selectionMatch':'','includeRuntime':true`}`}" 
+$uri
+try{
+    $response = Invoke-WebRequest -Uri $uri -Headers @{"Authorization"="Bearer $accessToken"}
+} catch {
+    $Error[0]
+    exit
+}
+
+$jsonResponse = $response.Content  | ConvertFrom-Json
+
+foreach($t in $jsonResponse.thermostatList)
+{
+    $t | fl *
+    $t.runtime | fl *
+
+}
---
+++ get-user-tokens.ps1
@@ -0,0 +1,41 @@
+﻿
+
+param
+(
+	    [Parameter(Mandatory = $true)] [string] $ApiKey
+	  , [Parameter(Mandatory = $false)] [string] $BaseUri = "https://api.ecobee.com/"
+)
+
+
+$uri = ("{0}authorize?response_type=ecobeePin&client_id={1}&scope=smartRead" -f $BaseUri,$ApiKey) 
+$uri
+try{
+    $response = Invoke-RestMethod -Uri $uri -Method Get
+} catch {
+    $Error[0]
+    exit
+}
+
+# -Method Post -Body $JSON -ContentType "application/json"
+
+# If you use Powershell 3, I know there have been some issues with Invoke-RestMethod, but you should be able to use Invoke-WebRequest as a replacement:
+
+# $response = Invoke-WebRequest -Uri "http://somesite.com/oneendpoint" -Method Post -Body $JSON 
+
+Read-Host ("Go to the ecobee portal and register for a new application with this PIN: {0}" -f $response.ecobeePin )
+
+
+$uri = ("{0}token?grant_type=ecobeePin&code={1}&client_id={2}" -f $BaseUri,$response.code,$ApiKey) 
+$uri
+try{
+    $response = Invoke-RestMethod -Uri $uri -Method Post
+} catch {
+    $Error[0]
+    exit
+}
+
+# Save the tokens
+$response | ConvertTo-Json | Out-File -FilePath ecobeeToken.json
+
+
+
